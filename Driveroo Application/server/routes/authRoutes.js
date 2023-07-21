f222d1cf0db522d3326bed00726289641e5444f5
Bloo87 <jinrongh21@gmail.com>
2023-07-21 17:05:13 -0400

    GetAll
    Made GetAllInstructor and GetAll Student

--- Driveroo Application/server/routes/authRoutes.js
+++ Driveroo Application/server/routes/authRoutes.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const { signupStudent, signupInstructor, login } = require('../controllers/loginController')
 
+const { getAllInstructors, getAllStudents } = require('../controllers/instructorsControllers');
+
 // auth/*
 router.post('/signupStudent', signupStudent);
 router.post('/signupInstructor', signupInstructor);
-
+router.get('/instructors', getAllInstructors); // get all instructors
+router.get('/students', getAllStudents); // get all students
 // login routes
 router.post('/login', login);
 
