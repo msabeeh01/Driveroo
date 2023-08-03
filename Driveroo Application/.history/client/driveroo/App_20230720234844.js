48a18c9dcd9dbf5d52fb67df5915633ea29864d3
Aimar007 <aimar.htv@gmail.com>
2023-08-03 12:37:10 -0400

    function to send recent messages

---
+++ Driveroo Application/.history/client/driveroo/App_20230720234844.js
@@ -0,0 +1,59 @@
+//dependencies
+import 'react-native-gesture-handler';
+
+import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider, Box, useTheme } from "native-base";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import axios from 'axios';
+
+//page imports
+import { AuthProvider, useAuth } from './src/context/AuthContext';
+import SigninStudent from './src/pages/SigninStudent';
+import TabNavigator from './src/navigation/TabNavigator';
+import theme from './src/theme/theme';
+import { color } from '@rneui/base';
+import AuthNavigator from './src/navigation/AuthNavigator';
+
+/*
+  CHANGE THIS TO YOUR IP ADDRESSS
+*/
+const yourIP = '10.0.0.67'
+axios.defaults.baseURL = `http://${yourIP}:3000`;
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <NativeBaseProvider theme={theme}>
+        <Layout />
+      </NativeBaseProvider>
+    </AuthProvider>
+  );
+}
+
+export const Layout = () => {
+  const { authState } = useAuth();
+  const theme = useTheme();
+
+  return (
+    <SafeAreaView style={[styles.container, {backgroundColor: theme.colors.blueGray[900]}]}>
+      <NavigationContainer>
+        <Stack.Navigator>{authState?.authenticated ?
+          <Stack.Screen name="Tab" component={TabNavigator} options={{ headerShown: false }} /> : 
+          <Stack.Screen name="AuthNavigator" component={AuthNavigator} options={{ headerShown: false }} />}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+    
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+});
